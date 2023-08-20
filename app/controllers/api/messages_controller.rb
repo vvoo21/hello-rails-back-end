7bf7aa6074306042313233306ccdabd909ba4ebc
vvoo21 <vaneolipa@gmail.com>
2023-08-19 22:52:29 -0500

    Add index and message method

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,8 @@
 class Api::MessagesController < ApplicationController
+    def index; end
+    
+    def message
+        @message = Message.order('RANDOM()').first
+        render json: @message
+    end
 end
