c2751d39d122020de939843dcdfe52ff4e3e1578
vvoo21 <vaneolipa@gmail.com>
2023-08-20 01:14:41 -0500

    Install gem 'rack-cors' and add config inside application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,12 @@ module HelloRailsBackEnd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete]
+      end
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
